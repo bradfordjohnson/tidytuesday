99e574065ebae7f04dbc06322b0f24067f025392
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-01-24 06:41:33 -0500

    Add files via upload

--- 2023/week-4/alone.R
+++ 2023/week-4/alone.R
@@ -1,6 +1,44 @@
 # load packages
 library(tidyverse)
-library(tidytuesdayR)
+
 
 # load data
-tuesdata <- tidytuesdayR::tt_load(2023, week = 4)
+loadouts <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-24/loadouts.csv')
+survivalists <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-01-24/survivalists.csv')
+
+# clean data
+loadouts <- loadouts |>
+  select(name, item_number, item)
+
+survivalists <- survivalists |>
+  select(name, days_lasted, result, season)
+
+joined_df <- survivalists |>
+  left_join(loadouts, by = c("name" = "name"))
+
+first_place <- joined_df |>
+  filter(result == 1)
+
+last_place <- joined_df |>
+  filter(result == 10 | result == 7 & season == 4) # season 4 had teams
+
+first_place_items <- first_place |>
+  group_by(item) |>
+  summarise(n = n()) |>
+  filter(n > 1) |>
+  arrange(desc(n))
+
+last_place_items <- last_place |>
+  group_by(item) |>
+  summarise(n = n()) |>
+  filter(n > 1) |>
+  arrange(desc(n))
+
+first_place_items <- first_place_items |>
+  mutate(place = "First")
+
+last_place_items <- last_place_items |>
+  mutate(place = "Last")
+
+items_bind <- first_place_items |>
+  rbind(last_place_items)
