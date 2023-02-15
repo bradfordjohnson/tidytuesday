af6a309300b9a24e876f9a368a8c8f9ecf575662
Ford Johnson <111791965+bradfordjohnson@users.noreply.github.com>
2023-02-15 06:53:02 -0500

    Add files via upload

--- 2023/week-7/age-gaps.R
+++ 2023/week-7/age-gaps.R
@@ -3,3 +3,10 @@ library(tidyverse)
 
 # load data
 age_gaps <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-02-14/age_gaps.csv')
+
+# wrangle data
+age_gaps <- age_gaps |>
+  mutate(relationship = case_when(
+    character_1_gender == "man" ~ "Older Man",
+    character_1_gender == "woman" ~ "Older Woman"
+    ))
