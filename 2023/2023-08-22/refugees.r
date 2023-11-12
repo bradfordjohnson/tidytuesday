9853a4ac953c7dc966529199daeb0241888edb23
bradfordjohnson <bradford.lewis.johnson@outlook.com>
2023-11-12 11:39:24 -0500

    updated plot for week 34

--- 2023/2023-08-22/refugees.r
+++ 2023/2023-08-22/refugees.r
@@ -10,7 +10,17 @@ font_add(family = "fb", regular = "assets/fontAwesome.otf")
 font_add_google(name = "Roboto", family = "Roboto")
 font_1 <- "Roboto"
 
-caption <- create_caption(2023, 34, background_color = "#000000", font = font_1)
+colors_json <- fromJSON("assets/colors.json")
+
+background_color <- colors_json$nord$snow_storm$nord6
+
+text_color <- colors_json$nord$polar_night$nord0
+
+color_pal <- c(colors_json$nord$frost$nord10, colors_json$nord$aurora$nord11, colors_json$nord$snow_storm$nord4, colors_json$nord$aurora$nord12)
+
+caption <- create_caption(2023, 34, background_color, font = font_1)
+
+title <- "<span>Refugee Trends:<span style='color:#D08770;'> USA </span>vs. Neighbors (Euclidean Distance)</span>"
 
 population <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2023/2023-08-22/population.csv")
 
@@ -44,18 +54,44 @@ selected_cols <- refugees_pivoted[, -c(1)]
 refugees_pivoted_with_distances <- euclidean_distance_df(refugees_pivoted, usa_array)
 
 refugees_long_with_distances <- refugees_pivoted_with_distances %>%
-  arrange(distance) %>%
-  head(7) %>%
-  pivot_longer(cols = -c(coa_name, distance), names_to = "year", values_to = "refugees") %>%
-  ungroup() %>%
-  mutate(color_flag = case_when(
-    coa_name == "United States of America" ~ "USA",
-    coa_name == "China" ~ "China",
-    coa_name == "France" ~ "France",
-    TRUE ~ "Other"
-  ))
+    arrange(distance) %>%
+    head(7) %>%
+    pivot_longer(cols = -c(coa_name, distance), names_to = "year", values_to = "refugees") %>%
+    ungroup() %>%
+    mutate(color_flag = case_when(
+        coa_name == "United States of America" ~ "USA",
+        coa_name == "China" ~ "China",
+        coa_name == "France" ~ "France",
+        TRUE ~ "Other"
+    ))
 
 refugees_long_with_distances %>%
     ggplot(aes(x = year, y = refugees, group = coa_name, color = color_flag)) +
     geom_line() +
-    theme_minimal()
+    labs(caption = caption, y = "Refugee Count", x = "", title = title) +
+    theme_void() +
+    scale_color_manual(values = color_pal) +
+    scale_y_continuous(labels = scales::comma) +
+    theme(
+        plot.background = element_rect(fill = background_color, color = background_color),
+        panel.background = element_rect(fill = background_color, color = background_color),
+        plot.caption = ggtext::element_textbox_simple(
+            margin = unit(c(6, 0, 0, 0), "mm"),
+            halign = 1,
+            color = text_color,
+            size = 5
+        ),
+        plot.title = ggtext::element_textbox_simple(
+            family = font_1,
+            margin = margin(0, 0, 2, 0, "mm"),
+            halign = .1,
+            size = 14,
+            color = text_color
+        ),
+        plot.margin = margin(5, 5, 5, 5, 'mm'),
+        legend.title = element_blank(),
+        panel.grid.major.y = element_line(color = colors_json$nord$polar_night$nord0, size = .1, linetype = "dotted"),
+        axis.text = element_text(color = text_color, family = font_1, size = 7),
+        axis.title.y = element_text(color = text_color, family = font_1, size = 7, angle = 90, margin = margin(0, 5, 0, 1, "mm")),
+        legend.text = element_text(color = text_color, family = font_1, size = 7),
+    )
