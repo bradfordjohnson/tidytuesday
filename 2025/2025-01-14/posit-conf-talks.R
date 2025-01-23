a910ab1389998a209cf2344850a89d3621374b43
bradfordjohnson <bradfordljohnson21+github@gmail.com>
2025-01-23 09:31:19 -0500

    Updated tidytuesday and visual for 2025-01-14

--- 2025/2025-01-14/posit-conf-talks.R
+++ 2025/2025-01-14/posit-conf-talks.R
@@ -2,6 +2,13 @@
 library(tidyverse)
 library(tidytext)
 library(ggplot2)
+library(htmltools)
+
+
+title <- paste0(
+  "Top Words by Frequency Shifts in <span style='color:#d08770;'>2023</span> vs <span style='color:#5e81ac;'>2024</span> posit::conf Titles"
+)
+
 
 conf2023 <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-01-14/conf2023.csv")
 conf2024 <- readr::read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/main/data/2025/2025-01-14/conf2024.csv")
@@ -30,26 +37,26 @@ word_comparison_long <- word_comparison %>%
   mutate(year = recode(year, "freq_2023" = "2023", "freq_2024" = "2024"))
 
 ggplot(word_comparison_long, aes(x = word, y = frequency, group = word, color = year)) +
-  geom_line(color = "black") +
-  geom_point() +
+  geom_line(color = "#4c566a") +
+  geom_point(aes(size = 1)) +
   coord_flip() +
-  scale_color_manual(values = c("2023" = "orange", "2024" = "blue")) +
+  scale_color_manual(values = c("2023" = "#d08770", "2024" = "#5e81ac")) +
   theme_minimal(base_family = "Verdana", base_size = 11) +
   labs(
-    title = "Top Words by Frequency Shifts in \n2023 vs 2024 posit::conf Titles",
+    title = title,
     y = "Frequency",
     caption = "Source: `posit::conf` talk's • Graphic: Ford Johnson"
   ) +
   theme(
     legend.title = element_blank(),
+    axis.text = element_text(size = 10),
     axis.title = element_blank(),
     panel.grid.minor = element_blank(),
-    legend.position = "top",
-    legend.justification = "left",
-    legend.margin = margin(0, 0, 0, 0, "mm"),
+    legend.position = "none",
+    plot.title = ggtext::element_textbox_simple(size = 15, hjust = 0, face = "plain", margin = margin(0, 0, 2, 0, "mm"), color = "black"),
     plot.margin = margin(20, 10, 10, 10),
     plot.background = element_rect(fill = "white", color = "white"),
     panel.background = element_rect(fill = "white", color = "white")
   )
 
-ggsave("test.png", scale = 1.5)
+ggsave("2025/2025-01-14/posit-conf-talks.png", scale = .75)
